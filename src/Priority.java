e4450296c41e808f9bb5272e3ec7f9de623a6e1c
jbt017 <jbt017@gmail.com>
2019-11-05 00:53:09 -0600

    started on priority queue

--- src/Priority.java
+++ src/Priority.java
@@ -1,7 +1,16 @@
 //Priority Queue Class
-public class Priority<Type> extends Queue<Type>{
+public class Priority<Book> extends Queue<Book>{
 
     Priority(){
         super();
     }
+
+    public void Insert(Book data, int priority){
+        super.First();
+        if(super.GetSize() != -1){
+        }else{
+            super.InsertAfter(data);
+        }
+    }
+
 }
